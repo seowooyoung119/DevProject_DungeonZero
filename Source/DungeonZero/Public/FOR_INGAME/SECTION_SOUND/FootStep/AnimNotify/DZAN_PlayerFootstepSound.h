485edb38bc2483f8b084cb75858e22ea94719c7b
seowooyoung <seohascome20th@naver.com>
2026-04-08 20:42:28 +0900

    누락 업로드

---
+++ Source/DungeonZero/Public/FOR_INGAME/SECTION_SOUND/FootStep/AnimNotify/DZAN_PlayerFootstepSound.h
@@ -0,0 +1,26 @@
+﻿// All CopyRight by BooZaGameStudio // 
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotify.h"
+#include "DZAN_PlayerFootstepSound.generated.h"
+
+/**
+ * // 플레이어 풋스텝 : 플레이어 전용으로 쓸 것
+ */
+UCLASS()
+class DUNGEONZERO_API UDZAN_PlayerFootstepSound : public UAnimNotify
+{
+	GENERATED_BODY()
+	
+public:
+	// 발 소켓 명
+	UPROPERTY(EditAnywhere, Category = "DZ | Footstep")
+	FName SocketName = TEXT("foot_l");
+	// 왼발인지 오른발인지 
+	UPROPERTY(EditAnywhere, Category = "DZ | Footstep")
+	bool bIsLeft = true;
+	
+	virtual void Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference) override;
+};
